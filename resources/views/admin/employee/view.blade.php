3ca13ede72fc4e5ca51441ffad46372961f77135
biiboy <biiboy@users.noreply.github.com>
2024-06-24 13:55:44 +0700

    Fix Employee Page

--- resources/views/admin/employee/view.blade.php
+++ resources/views/admin/employee/view.blade.php
@@ -18,7 +18,7 @@
             <div class="card-datatable table-responsive pt-0 mt-3">
                 <table class="datatables-basic table cell-border" id="datatables-basic">
                     <thead>
-                        <tr>
+                        <tr align="center">
                             <th>No</th>
                             <th>Nama Karyawan</th>
                             <th>NIK</th>
@@ -31,20 +31,27 @@
                     <tbody>
                         @foreach ($employees as $employee)
                             <tr>
-                                <td>{{ $loop->iteration }}</td>
+                                <td><center>{{ $loop->iteration }}<center></td>
                                 <td>{{ $employee->name ?? '' }}</td>
-                                <td>{{ $employee->nik }}</td>
-                                <td>{{ ucfirst(strtolower($employee->jenis_kelamin)) }}</td>
-                                <td>{{ $employee->branch->lokasi }}</td>
-                                <td>
+                                <td><center>{{ $employee->nik }}</center></td>
+                                <td><center>
+                                    @if ($employee->jenis_kelamin == 'PEREMPUAN')
+                                        <span class="py-1 px-2 rounded-full text-xs bg-success text-white cursor-pointer font-medium">Perempuan</span>
+                                    @else
+                                        <span class="py-1 px-2 rounded-full text-xs bg-danger text-white cursor-pointer font-medium">Laki - Laki</span>
+                                    @endif </center>
+                                </td>
+                                {{-- <td>{{ ucfirst(strtolower($employee->jenis_kelamin)) }}</td> --}}
+                                <td><center>{{ $employee->branch->lokasi }}</center></td>
+                                <td><center>
                                     @if ($employee->status == 'Aktif')
                                         <span class="text-success">Aktif</span>
                                     @else
                                         <span class="text-danger">Tidak Aktif</span>
-                                    @endif
+                                    @endif </center>
                                 </td>
-                                <td>
-                                    <a href="{{ route('employee.detail', $employee->id) }}" class="btn btn-sm btn-primary">Detail</a>
+                                <td><center>
+                                    <a href="{{ route('employee.detail', $employee->id) }}" class="btn btn-sm btn-primary">Detail</a></center>
                                 </td>
                             </tr>
                         @endforeach
